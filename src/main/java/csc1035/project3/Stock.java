35d6082e0db1110a1833c312e1d2f4f0a38bf17f
b9012949 <b9012949@newcastle.ac.uk>
2020-02-26 11:29:54 +0000

    Implemented EPOS interface

--- src/main/java/csc1035/project3/Stock.java
+++ src/main/java/csc1035/project3/Stock.java
@@ -1,4 +1,23 @@
 package csc1035.project3;
 
-public class Stock {
+public class Stock implements EPOS {
+    @Override
+    public void countStock() {
+
+    }
+
+    @Override
+    public void customerTransaction() {
+
+    }
+
+    @Override
+    public void generateReceipt() {
+
+    }
+
+    @Override
+    public void updateStock() {
+
+    }
 }
