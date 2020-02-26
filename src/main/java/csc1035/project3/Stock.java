4f93ec0db988c9b3cb13f75c08bd45660a0f9bf9
b9012949 <b9012949@newcastle.ac.uk>
2020-02-26 11:55:46 +0000

    Implemented code for the Columns in the stock table. Including generated Id value

--- src/main/java/csc1035/project3/Stock.java
+++ src/main/java/csc1035/project3/Stock.java
@@ -1,7 +1,36 @@
 package csc1035.project3;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Stock")
+
 public class Stock implements EPOS {
-    // Test
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "sid", updatable = false, nullable = false)
+    private final int id;
+
+    @Column(name = "stock_name")
+    private String name;
+
+    @Column(name = "stock_category")
+    private String category;
+
+    @Column(name = "stock_perishable")
+    private String perishable;
+
+    @Column (name = "stock_cost")
+    private String cost;
+
+    @Column (name = "stock_remaining_stock")
+    private String remaining_stock;
+
+    @Column (name = "stock_sell_price")
+    private String sell_price;
+
+
 
     @Override
     public void countStock() {
