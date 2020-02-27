eaaca632d7e9983d3de5b21eb3256145f87ee9e2
b9035777 <P.Georgiev2@newcastle.ac.uk>
2020-02-27 14:34:02 +0000

    Created a Stock table and populated it

--- src/main/java/csc1035/project3/CRUD/Create.java
+++ src/main/java/csc1035/project3/CRUD/Create.java
@@ -1,21 +1,32 @@
 package csc1035.project3.CRUD;
+import org.hibernate.Session;
 
-
+import csc1035.project3.HibernateUtil;
 import csc1035.project3.Stock;
 
 public class Create {
 
     Stock fortnite;
+    Stock apex;
 
     public Create() {
-        fortnite = new Stock(1, "fortnite", "videogame", false, 99.99f, 10,
+
+        fortnite = new Stock(1, "Fortnite", "Video_Games", false, 99.99f, 10,
                 12.99f);
+
+        apex = new Stock(2, "Apex", "Video_Games", false, 89.99f, 8,
+                9.99f);
+
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        session.beginTransaction();
+        session.save(fortnite);
+        session.save(apex);
+        session.getTransaction().commit();
+        session.close();
     }
 
     public Stock getFortnite() {
         return fortnite;
     }
-
-
 
 }
--- src/main/java/csc1035/project3/Main.java
+++ src/main/java/csc1035/project3/Main.java
@@ -10,7 +10,6 @@ public class Main {
 
         System.out.println(create.getFortnite().getId());
 
-
     }
 
 }
