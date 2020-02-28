e66b94fc18f26d7e301330c78457e3c38e4e902a
b9012949 <b9012949@newcastle.ac.uk>
2020-02-28 14:46:10 +0000

    Added method to check for duplicates when adding new items to the stock

--- src/main/java/csc1035/project3/CRUD.java
+++ src/main/java/csc1035/project3/CRUD.java
@@ -2,8 +2,9 @@ package csc1035.project3;
 
 import org.hibernate.Session;
 
-import csc1035.project3.HibernateUtil;
-import csc1035.project3.Stock;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class CRUD {
 
@@ -11,11 +12,34 @@ public class CRUD {
         Stock stockToAdd = new Stock(name, category, perishable, cost, remaining_stock, sell_price);
         Session session = HibernateUtil.getSessionFactory().openSession();
         session.beginTransaction();
-        session.save(stockToAdd);
-        session.getTransaction().commit();
-        session.close();
+        if (!checkDuplicates()) {
+            session.save(stockToAdd);
+            session.getTransaction().commit();
+            session.close();
+        } else {
+          System.out.println("Item already in stock");
+        }
+    }
+
+    public boolean checkDuplicates() {
+
+        List stock = session.createQuery("FROM Stock").list();
+        ArrayList<String> checkedNames = new ArrayList<String>();
+
+        for (Iterator<Stock> iterator = names.iterator(); iterator.hasNext();) {
+            Stock item = (Stock) iterator.next();
+            if (checkedNames.contains(item.getName())) {
+                return true;
+            }
+            checkedNames.add(item.getName());
+
+        }
+        return false;
+
 
     }
+
+
 
 
 }
