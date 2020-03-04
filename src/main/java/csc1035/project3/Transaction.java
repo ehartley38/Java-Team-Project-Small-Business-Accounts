036a3a9f86157175f516d30a37199f0de84e17c3
b9026631 <c.laking1@newcastle.ac.uk>
2020-03-04 11:16:22 +0000

    Changed Transaction class so that it no longer implements EPOS.

--- src/main/java/csc1035/project3/Transaction.java
+++ src/main/java/csc1035/project3/Transaction.java
@@ -5,7 +5,7 @@ import org.hibernate.Session;
 import javax.persistence.*;
 
 @Entity(name = "Transaction") // Table name
-public class Transaction implements EPOS{
+public class Transaction {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,7 +21,7 @@ public class Transaction implements EPOS{
     @Column
     private float change_given;
 
-    public Transaction(float cost, float money_given, float change_given){
+    public Transaction(float cost, float money_given, float change_given) {
         this.cost = cost;
         this.money_given = money_given;
         this.change_given = change_given;
@@ -59,33 +59,22 @@ public class Transaction implements EPOS{
         this.change_given = change_given;
     }
 
-    @Override
-    public void countStock() {
-
-    }
-
-    @Override
-    public void addCustomerTransaction(){
+    public void addCustomerTransaction() {
         Session session = HibernateUtil.getSessionFactory().openSession();
-        try{
-        session.beginTransaction();
-        session.save(this);
-        session.getTransaction().commit();
-    } catch (HibernateException e) {
-        if (session != null) session.getTransaction().rollback();
-        e.printStackTrace();
-    } finally {
-        session.close();
+        try {
+            session.beginTransaction();
+            session.save(this);
+            session.getTransaction().commit();
+        } catch (HibernateException e) {
+            if (session != null) session.getTransaction().rollback();
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
     }
-}
 
-    @Override
     public void generateReceipt() {
 
     }
-
-    @Override
-    public void updateStock() {
-
-    }
 }
+
